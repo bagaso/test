6f8d653a52cc4e34629ef94f180f13ad097952bb
mp3sniff <mp3sniff@gmail.com>
2017-01-25 17:02:00 +0800

    Refresh

---
+++ database/factories/ModelFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| Here you may define all of your model factories. Model factories give
+| you a convenient way to create models for testing and seeding your
+| database. Just tell the factory how a default model should look.
+|
+*/
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+use Illuminate\Support\Facades\Hash;
+
+$factory->define(App\User::class, function (Faker\Generator $faker) {
+    static $password;
+
+    return [
+        'username' => str_random(3) . '.' . str_random(5),
+        'email' => str_random(5) . '@' . str_random(5) . '.com',
+        'password' => '12345',
+        'fullname' => $faker->firstName . ' ' . $faker->lastName,
+        'user_group_id' => 5,
+        'status_id' => 1,
+        'remember_token' => str_random(10),
+    ];
+});
