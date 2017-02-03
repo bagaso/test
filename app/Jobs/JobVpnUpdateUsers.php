aa534e8851805c29e124e37fa45800941becaeda
mp3sniff <mp3sniff@gmail.com>
2017-02-04 01:57:30 +0800

    refresh

--- app/Jobs/JobVpnUpdateUsers.php
+++ app/Jobs/JobVpnUpdateUsers.php
@@ -33,7 +33,7 @@ class JobVpnUpdateUsers implements ShouldQueue
         $logs = $this->parseLog('http://' . strtolower($this->server->server_domain) . '/logs/logs.log', 'tcp');
         foreach($logs as $log)
         {
-            $users = $this->server->users->has('vpn')->where('username', $log['CommonName'] ? $log['CommonName'] : 'UNDEF')->get();
+            $users = $this->server->users->where('username', $log['CommonName'] ? $log['CommonName'] : 'UNDEF')->has('vpn')->get();
             foreach($users as $user)
             {
                 $user->vpn->byte_sent = intval($log['BytesSent']) ? intval($log['BytesSent']) : 0;
