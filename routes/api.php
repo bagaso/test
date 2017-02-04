14543d9c9e272cfc46643fe259d37685103de7ce
mp3sniff <mp3sniff@gmail.com>
2017-02-04 14:58:33 +0800

    refresh

--- routes/api.php
+++ routes/api.php
@@ -68,7 +68,7 @@ Route::get('/vpn_auth_connect', function (Request $request) {
 Route::get('/vpn_auth_disconnect', function (Request $request) {
     $username = trim($request->username);
     $delete = \App\User::where('username', $username);
-    if($delete->vpn) {
+    if($delete->first()->vpn->count() > 0) {
         $delete->first()->vpn->delete();
     }
     return '1';
