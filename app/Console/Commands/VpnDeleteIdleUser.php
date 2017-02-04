4aa5247d23e342bc6c3b845655087adab7877367
mp3sniff <mp3sniff@gmail.com>
2017-02-04 15:51:14 +0800

    refresh

--- app/Console/Commands/VpnDeleteIdleUser.php
+++ app/Console/Commands/VpnDeleteIdleUser.php
@@ -38,7 +38,7 @@ class VpnDeleteIdleUser extends Command
      */
     public function handle()
     {
-        $delete_idle = \App\OnlineUser::where('updated_at', '<=', \Carbon\Carbon::now()->subMinutes(5));
+        $delete_idle = \App\OnlineUser::where('updated_at', '<=', \Carbon\Carbon::now()->subMinutes(2));
         foreach ($delete_idle as $online_user) {
             $job = (new JobVpnDisconnectUser($online_user->user->username, $online_user->server_ip, $online_user->server_port))->delay(\Carbon\Carbon::now()->addSeconds(5))->onQueue('disconnectvpnuser');
             dispatch($job);
