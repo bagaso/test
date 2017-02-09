118de07a60094ccda0c9ddd0deb5d72509889c9c
mp3sniff <mp3sniff@gmail.com>
2017-02-09 23:28:18 +0800

    refresh

--- routes/api.php
+++ routes/api.php
@@ -113,7 +113,7 @@ Route::get('/vpn_auth_disconnect', function (Request $request) {
 
     if($current->gte($dt)) {
         $vpn = $user_delete->vpn()->where('vpn_server_id', $server->id)->firstorfail();
-        $user_delete->consumable_data = ($vpn->data_available - $vpn->getOriginal('byte_sent') >= 0 ? $vpn->data_available - $vpn->getOriginal('byte_sent') : 0;
+        $user_delete->consumable_data = ($vpn->data_available - $vpn->getOriginal('byte_sent')) >= 0 ? $vpn->data_available - $vpn->getOriginal('byte_sent') : 0;
         $user_delete->timestamp = false;
         $user_delete->save();
     }
