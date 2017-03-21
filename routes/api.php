629443387ed3b818bc10f91668cecba1d7d063d5
mp3sniff <mp3sniff@gmail.com>
2017-03-21 11:16:09 +0800

    refresh

--- routes/api.php
+++ routes/api.php
@@ -34,9 +34,9 @@ Route::get('/vpn_auth', function (Request $request) {
         $password = $request->password;
         $server_key = $request->server_key;
 
-        if (!ctype_lower($username)) {
-            return '0';
-        }
+//        if (!ctype_lower($username)) {
+//            return '0';
+//        }
 
         $server = \App\VpnServer::where('server_key', $server_key)->firstorfail();
 
