3f5d5741c44fd1bd537150911e134c5a25d7ee10
mp3sniff <mp3sniff@gmail.com>
2017-03-25 16:52:34 +0800

    refresh

--- app/Http/Controllers/ManageUser/CreateUserController.php
+++ app/Http/Controllers/ManageUser/CreateUserController.php
@@ -101,7 +101,7 @@ class CreateUserController extends Controller
         $new_user->email = $request->email;
         $new_user->fullname = $request->fullname;
         $new_user->status_id = $request->status_id;
-        $new_user->consumable_data = 1073741824;
+        $new_user->consumable_data = $site_settings->settings['consumable_data'] * 1048576;
         $new_user->expired_at = $current->addSeconds($site_settings->settings['trial_period']);
         $new_user->save();
         if(in_array($new_user->user_group_id, [2,3,4])) {
