a6a91a4d8cc23fa5553a5e5e3045e8f0b1e587bd
mp3sniff <mp3sniff@gmail.com>
2017-03-30 23:04:43 +0800

    refresh

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -34,16 +34,16 @@ class Kernel extends ConsoleKernel
         $schedule->command('vpnuser')->everyMinute();
         $schedule->command('vpn:deleteidle')->everyMinute();
         $schedule->command('vpn:monitoruser')->everyMinute();
-//        if(Schema::hasTable('site_settings') && SiteSetting::where('id', 1)->exists()) {
-//            $site_settings = SiteSettings::find(1);
-//            if($site_settings->settings['data_reset']==0) {
-//                $schedule->command('vpn:resetdata')->daily();
-//            } else if($site_settings->settings['data_reset']==1) {
-//                $schedule->command('vpn:resetdata')->weekly();
-//            } else if($site_settings->settings['data_reset']==2) {
-//                $schedule->command('vpn:resetdata')->monthly();
-//            }
-//        }
+        if(Schema::hasTable('site_settings') && SiteSetting::where('id', 1)->exists()) {
+            $site_settings = SiteSettings::find(1);
+            if($site_settings->settings['data_reset']==0) {
+                $schedule->command('vpn:resetdata')->daily();
+            } else if($site_settings->settings['data_reset']==1) {
+                $schedule->command('vpn:resetdata')->weekly();
+            } else if($site_settings->settings['data_reset']==2) {
+                $schedule->command('vpn:resetdata')->monthly();
+            }
+        }
         $dt = \Carbon\Carbon::now()->toDateString() . '_' . \Carbon\Carbon::now()->toTimeString();
         $schedule->command("db:backup --database=mysql --destination=dropbox --destinationPath={$dt} --compression=gzip")->twiceDaily(1,12);
     }
