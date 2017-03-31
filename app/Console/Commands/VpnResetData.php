7f0a4d80f55c8b783eb0718384ed21a1e7d7d39b
mp3sniff <mp3sniff@gmail.com>
2017-03-31 13:58:22 +0800

    refresh

--- app/Console/Commands/VpnResetData.php
+++ app/Console/Commands/VpnResetData.php
@@ -40,7 +40,7 @@ class VpnResetData extends Command
      */
     public function handle()
     {
-        if(Schema::hasTable('site_settings')) {
+        if(Schema::hasTable('site_settings') && SiteSettings::where('id', 1)->exists()) {
             $site_settings = SiteSettings::find(1);
             User::query()->update(['consumable_data' => $site_settings->settings['consumable_data'] * 1048576]);
         }
