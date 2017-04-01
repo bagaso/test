479450c4bbc5234739f1953c0dfd9a197e9073a8
mp3sniff <mp3sniff@gmail.com>
2017-04-01 10:10:19 +0800

    refresh

--- app/Console/Commands/VpnDeleteIdleUser.php
+++ app/Console/Commands/VpnDeleteIdleUser.php
@@ -40,9 +40,9 @@ class VpnDeleteIdleUser extends Command
     {
         $delete_idle = \App\OnlineUser::with(['user', 'vpnserver'])->where('updated_at', '<=', \Carbon\Carbon::now()->subMinutes(5));
         foreach ($delete_idle->get() as $online_user) {
-//            $job = (new JobVpnDisconnectUser($online_user->user->username, $online_user->vpnserver->server_ip, $online_user->vpnserver->server_port))->delay(\Carbon\Carbon::now()->addSeconds(5))->onQueue('disconnectvpnuser');
-//            dispatch($job);
-            if(!$online_user->user->isAdmin() && $online_user->vpnserver->limit_bandwidth && $online_user->data_available > 0) {
+            $job = (new JobVpnDisconnectUser($online_user->user->username, $online_user->vpnserver->server_ip, $online_user->vpnserver->server_port))->delay(\Carbon\Carbon::now()->addSeconds(5))->onQueue('disconnectvpnuser');
+            dispatch($job);
+            if(!is_null($online_user->user) && !$online_user->user->isAdmin() && $online_user->vpnserver->limit_bandwidth && $online_user->data_available > 0) {
                 $online_user->user->timestamps = false;
                 $data = $online_user->data_available - floatval($online_user->byte_sent);
                 $online_user->user->consumable_data = ($data >= 0) ? $data : 0;
