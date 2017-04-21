57523e2efd2052966aa5586871a0041bfe63af89
mp3sniff <mp3sniff@gmail.com>
2017-04-21 16:38:36 +0800

    refresh

--- app/Jobs/JobVpnUpdateUsers.php
+++ app/Jobs/JobVpnUpdateUsers.php
@@ -42,9 +42,9 @@ class JobVpnUpdateUsers implements ShouldQueue
                 foreach($logs as $log)
                 {
                     try {
-                        $user = \App\User::where('username', $log['CommonName'])->firstorfail();
+                        $user = \App\User::with('user_package')->where('username', $log['CommonName'])->firstorfail();
                         $login_session = $user->vpn->count();
-                        if($user->isAdmin() || $login_session >= 1 && $login_session <= $user->vpn_session) {
+                        if($user->isAdmin() || $login_session >= 1 && $login_session <= intval($user->user_package->user_package['device'])) {
                             $vpn_user = $user->vpn()->where('vpn_server_id', $this->server_id);
                             $vpn_user->update(['byte_sent' => floatval($log['BytesSent']) ? floatval($log['BytesSent']) : 0, 'byte_received' => floatval($log['BytesReceived']) ? floatval($log['BytesReceived']) : 0]);
                             //$vpn_user->update(['byte_sent' => 0, 'byte_received' => 0]);
