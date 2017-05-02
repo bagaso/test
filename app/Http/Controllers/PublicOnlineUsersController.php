daaf60a4aa5f32d5e3abeb3958590aa34e06550c
mp3sniff <mp3sniff@gmail.com>
2017-05-03 01:48:11 +0800

    refresh

--- app/Http/Controllers/PublicOnlineUsersController.php
+++ app/Http/Controllers/PublicOnlineUsersController.php
@@ -40,7 +40,7 @@ class PublicOnlineUsersController extends Controller
             ], 403);
         }
 
-        $data = OnlineUser::with('user', 'vpnserver')->orderBy('created_at', 'desc')->paginate(1);
+        $data = OnlineUser::with('user', 'vpnserver')->orderBy('created_at', 'desc')->paginate(50);
 
         $site_options['site_name'] = $db_settings->settings['site_name'];
         $site_options['sub_name'] = 'Online Users';
